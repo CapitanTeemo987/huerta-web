c9c67f1cb348975c22b8edf07239861f47a577ce
Valentino Ortiz <A01712296@tec.mx>
2026-07-04 23:21:53 -0600

    feat: navbar implemented

---
+++ frontend/components/navbar.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="w-full bg-white shadow-md">
+      <div className="max-w-7xl mx-auto flex justify-between items-center p-4">
+
+        <h1 className="text-2xl font-bold text-green-700">
+          Aguacates Zicuin
+        </h1>
+
+        <ul className="flex gap-8 text-gray-700 font-medium">
+            <li>
+                <Link href="/">Inicio</Link>
+            </li>
+
+            <li>
+                <Link href="/nosotros">Nosotros</Link>
+            </li>
+
+            <li>
+                <Link href="/productos">Productos</Link>
+            </li>
+
+            <li>
+                <Link href="/contacto">Contacto</Link>
+            </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
