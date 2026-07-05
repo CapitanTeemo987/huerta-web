6190c48e76086572e8958deb9d324a1999e0b242
Valentino Ortiz <A01712296@tec.mx>
2026-07-05 16:10:16 -0600

    fix: navbar font size components changed

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="w-full bg-white shadow-md">
+    <nav className="w-full bg-white border-b border-gray-200">
       <div className="max-w-7xl mx-auto flex justify-between items-center p-4">
 
         <h1 className="text-2xl font-bold text-green-700">
           Aguacates Zicuin
         </h1>
 
-        <ul className="flex gap-8 text-gray-700 font-medium">
+        <ul className="flex gap-8 text-lg text-gray-700 font-medium">
             <li>
                 <Link href="/">Inicio</Link>
             </li>
